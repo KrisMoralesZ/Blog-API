7e3b61d667555549338c76741dcd824f894881e8
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-11-14 11:45:35 -0700

    Create Get User Controllers and Services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from 'src/models/user.model';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(): User[] {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  findUser(@Param('id') id: number): User | undefined {
+    return this.usersService.getUserById(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/models/user.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    { id: 1, name: 'Alice', email: 'alice@example.com' },
+    { id: 2, name: 'Bob', email: 'bob@example.com' },
+    { id: 3, name: 'Charlie', email: 'charlie@example.com' },
+  ];
+
+  getAllUsers(): User[] {
+    return this.users;
+  }
+
+  getUserById(id: number): User | undefined {
+    return this.users.find((user) => user.id === id);
+  }
+}
