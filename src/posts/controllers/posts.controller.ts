943f1ff35ddb4ed41eb5b1a420ea37b4d480c702
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-12-10 12:49:56 -0700

    Fix Folder location

--- src/posts/controllers/posts.controller.ts
+++ src/posts/controllers/posts.controller.ts
@@ -7,7 +7,7 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
-import { PostsService } from '../../services/posts.service';
+import { PostsService } from '../services/posts.service';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { UpdatePostDto } from '../dto/update-post.dto';
 
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PostsService } from '../services/posts.service';
+import { PostsService } from './services/posts.service';
 import { PostsController } from './controllers/posts.controller';
 import { CategoriesController } from './controllers/categories/categories.controller';
+import { CategoriesService } from './services/categories/categories.service';
 
 @Module({
   controllers: [PostsController, CategoriesController],
-  providers: [PostsService],
+  providers: [PostsService, CategoriesService],
 })
 export class PostsModule {}
