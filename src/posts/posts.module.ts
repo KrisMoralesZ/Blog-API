329dd2af3766c5c679adcf1656884bc95666ac52
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-12-10 12:42:45 -0700

    Move Services to dedicated Folder

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PostsService } from './posts.service';
+import { PostsService } from '../services/posts.service';
 import { PostsController } from './controllers/posts.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
 
 @Module({
-  controllers: [PostsController],
+  controllers: [PostsController, CategoriesController],
   providers: [PostsService],
 })
 export class PostsModule {}
