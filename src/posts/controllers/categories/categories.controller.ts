383734b55f6ca1da21d1f4a39be5aae3b6211644
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-12-10 20:53:04 -0700

    Handle Categories

--- src/posts/controllers/categories/categories.controller.ts
+++ src/posts/controllers/categories/categories.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateCategoryDto } from 'src/posts/dto/create-category.dto';
+import { UpdateCategoryDto } from 'src/posts/dto/update-category.dto';
 import { CategoriesService } from 'src/posts/services/categories/categories.service';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoriesService.createCategory(createCategoryDto.name);
+  }
+
+  @Get()
+  getAllCategories() {
+    return this.categoriesService.getAllCategories();
+  }
+
+  @Get(':id')
+  getCategoryById(@Param('id') id: number) {
+    return this.categoriesService.getCategoryById(id);
+  }
+
+  @Put(':id')
+  updateCategory(
+    @Param('id') id: number,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.updateCategory(
+      id,
+      updateCategoryDto.name || '',
+    );
+  }
+
+  @Delete(':id')
+  deleteCategory(@Param('id') id: number) {
+    return this.categoriesService.deleteCategory(id);
+  }
 }
