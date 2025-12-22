39f95eef0f88fd173cc2602016c6815442fe048b
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-12-21 23:34:02 -0700

    Add Strategy

--- src/posts/controllers/posts.controller.ts
+++ src/posts/controllers/posts.controller.ts
@@ -6,7 +6,9 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { PostsService } from '@posts/services/posts.service';
 import { CreatePostDto } from '@posts/dto/create-post.dto';
 import { UpdatePostDto } from '@posts/dto/update-post.dto';
@@ -15,6 +17,7 @@ import { UpdatePostDto } from '@posts/dto/update-post.dto';
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.createPost(createPostDto);
@@ -30,11 +33,13 @@ export class PostsController {
     return this.postsService.getPostById(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postsService.updatePost(+id, updatePostDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.postsService.deletePost(+id);
